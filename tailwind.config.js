0450f09b188cc8b9a179fff384145abcde061b1b
mikolajczykb <uncharted.mikolajczyk4@gmail.com>
2021-05-08 23:40:35 +0200

    bug fixes, added README to frontend side of things

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   mode: 'jit',
   purge: [
-      './templates/tudutul-webapp/*.html'
+      './tudutul_webapp/templates/tudutul-webapp/*.html'
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
