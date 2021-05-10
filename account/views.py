048d0035e3b7b9a51d46bf72f26af6c94aeb0959
mikolajczykb <uncharted.mikolajczyk4@gmail.com>
2021-05-10 19:33:45 +0200

    change back account/views.py

--- account/views.py
+++ account/views.py
@@ -1,28 +1,28 @@
 from django.shortcuts import redirect
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.renderers import TemplateHTMLRenderer, JSONRenderer
+from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.decorators import api_view
 from .forms import RegisterForm
 
-    
 
 class RegistrationView(APIView):
-    renderer_classes = [JSONRenderer]
-    
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'register/register.html'
+
     def get(self, request):
         return Response()
 
     def post(self, request):
-        print(request.POST); 
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            return Response(data = {"ans": "Success"})
+            return redirect('/')
         else:
             answer = form.reason()
-        
-        return Response(data = {"ans": answer})
+
+        return Response(data={"ans": answer})
+
 
 class LoginView(APIView):
 
@@ -31,6 +31,7 @@ class LoginView(APIView):
 
     def post(self, request):
         pass
+
 
 class AccountView(APIView):
 
