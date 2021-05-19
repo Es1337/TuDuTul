2331e6c8ead3dd26adae14d61a87d445711552e0
mikolajczykb <uncharted.mikolajczyk4@gmail.com>
2021-05-19 23:11:55 +0200

    change html structure, files should inherit after base template

--- tudutul_webapp/views.py
+++ tudutul_webapp/views.py
@@ -3,17 +3,17 @@ from django.http import HttpResponse
 
 
 def index(request, *args, **kwargs):
-    return render(request, 'tudutul-webapp/index.html')
+    return render(request, 'index.html')
 # Create your views here.
 
 
-def login(request, *args, **kwargs):
-    return render(request, 'tudutul-webapp/login.html')
-
-
-def register(request, *args, **kwargs):
-    return render(request, 'tudutul-webapp/register.html')
-
-
-def offline(request, *args, **kwargs):
-    return render(request, 'tudutul-webapp/offline-notes.html')
+# def login(request, *args, **kwargs):
+#     return render(request, 'tudutul-webapp/login.html')
+#
+#
+# def register(request, *args, **kwargs):
+#     return render(request, 'tudutul-webapp/register.html')
+#
+#
+# def offline(request, *args, **kwargs):
+#     return render(request, 'tudutul-webapp/offline-notes.html')
